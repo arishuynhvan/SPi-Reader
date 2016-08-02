357e654478fb6efd0f2622f3c19750920d96525b
ravishwetha <shwetha.ravi3@gmail.com>
2016-08-02 15:19:00 +0800

    $SPATH
    Detects user's sonic-pi-cli gem location

--- blog/app/controllers/SPi.sh
+++ blog/app/controllers/SPi.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-ls
 SPATH=$(which sonic_pi)
-echo $SPATH
-cd /${SPATH:1:${#SPATH}-9}
-pwd
-ls
-/Users/ravi/.rvm/gems/ruby-2.3.0/bin/sonic_pi play 70
+echo 'Your sonic-pi-cli path:' $SPATH
+$SPATH play 70
