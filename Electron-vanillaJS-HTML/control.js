58c7a34f47276ac8c09e67a4bf0622bd8e0902c9
ravishwetha <shwetha.ravi3@gmail.com>
2016-08-15 16:58:23 +0800

    Remove extra directories, play button
    Submit button now stores code and play button runs code through
    sonic-pi server instead of both being handled by submit button.

--- Electron-vanillaJS-HTML/control.js
+++ Electron-vanillaJS-HTML/control.js
@@ -1,27 +1,32 @@
-
+// Functions associated with forms and buttons on index.html are defined here.
 
 var num=0;
 var storeNode;
-function submit(){
 
-//Store the value in the text-field, which is the input from the user, into a variable
+function submit(){
+    //Store the value in the text-field, which is the input from the user, into a variable
 	var storeCode = document.getElementById('text_field').value;
 	
 	if(storeCode.length!==0){
 		storeNode = document.createTextNode(storeCode);
 		document.getElementById('buffer1').appendChild(storeNode);
 	} 
-	sendServer(storeCode);
 }
 
 function del(){
 	storeNode.parentNode.removeChild(storeNode);
 }
 
-function sendServer(storeCode) {
+function sendServer() {
+  if(storeNode.length == null || storeNode.length == 0) {
+	  console.log('buffer is empty');
+	  return;
+  }
+
+  console.log('playing: ' + storeNode.nodeValue);
   var exec = require('child_process').exec, child;
 
-  child = exec('./client/SPi-reader.rb ' + storeCode,
+  child = exec('./client/SPi-reader.rb ' + storeNode.nodeValue,
       function (error, stdout, stderr) {
 	  console.log('stdout: ' + stdout);
 	  console.log('stderr: ' + stderr);
