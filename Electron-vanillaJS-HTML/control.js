a03ada77b6976dbd8d3d4209c58f21f668099b17
Tu An Huynh Van <arishuynhvan@gmail.com>
2016-08-21 09:22:40 +0800

    Fixed small issue for pause

--- Electron-vanillaJS-HTML/control.js
+++ Electron-vanillaJS-HTML/control.js
@@ -13,13 +13,13 @@ shortcut.add("Ctrl+B",
 var msg = new SpeechSynthesisUtterance('Hello World');
     window.speechSynthesis.speak(msg);
    	window.speechSynthesis.resume();
-
+var isPaused=true;
 if('speechSynthesis' in window){
 	var synth = window.speechSynthesis;
 	var input;
 	
 	var speech;
-	var isPaused=true;
+	
 	console.log('there is speechSynthesis');
 	shortcut.bind('ctrl+alt', function(e){speechControl(e)});
 
@@ -44,7 +44,11 @@ function parseSpeech(input){
 */
 function speechControl(e){
 	console.log('play/pause');
-		if(!isPaused){synth.pause();synth.cancel();}
+		if(!isPaused){
+			isPause=false;
+			synth.pause();
+			synth.cancel();
+		}
 		else{
 			console.log('preparing speech');
 		input = document.activeElement.value;//document.getElementById("code").value;
