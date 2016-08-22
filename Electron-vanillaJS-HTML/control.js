98f1c71343587445ba4c170df5f4875480e42eaf
ravishwetha <shwetha.ravi3@gmail.com>
2016-08-22 22:17:53 +0800

    Format Commands

--- Electron-vanillaJS-HTML/control.js
+++ Electron-vanillaJS-HTML/control.js
@@ -1,3 +1,4 @@
+//<<<<<<< Updated upstream
 /**@module*/
 // Functions associated with forms and buttons on index.html are defined here.
 var shortcut=require('./mousetrap.js');
@@ -13,49 +14,59 @@ function control(){
 	}
 	else
 		alert("This window doesn't support Speech Synthesis API");
+}
 
 	/**This function will extract the input from the start to the end of the line where the cursor is
 	Also it limits the return string to have 300 or lesscharacters
 	It replaces symbols with their English texts
 	@param: input
 	*/
-	function parseSpeech(input){
-		var parsedSpeech=input;
-		
-		var specialChar=[".","(",")",":", ";", 
-		"{", "}", "$", "?",
-		"!", "<", ">", "=", "\\", "/"];
-		
-		var spellOut=[" period ", " left bracket ", " right bracket ", " colon ", " semi-colon ",
-		" left curly bracket ", " right curly bracket ", " dollar sign ", " question mark ", 
-		" exclamation mark ", " less than ", " more than ", " is ", " slash ", " backslash "];
-		
-		specialChar.map(function(char,idx){
-			var delimStart=parsedSpeech.indexOf(char);
-			while(delimStart>-1){
-				if(char == "!" && parsedSpeech[delimStart+1] == "="){
-					console.log(parsedSpeech.substr(delimStart, 2));
-					parsedSpeech=parsedSpeech.replace(parsedSpeech.substr(delimStart, 2)," not equal to ");
-				}else if(char === "=" && parsedSpeech[delimStart+1] === "="){
-					var length = (parsedSpeech.indexOf("=",delimStart+2) === delimStart+2)? 3:2;
-					console.log(parsedSpeech.substr(delimStart, length));
-					parsedSpeech=parsedSpeech.replace(parsedSpeech.substr(delimStart, length)," equal to ");
-				}
-				else parsedSpeech=parsedSpeech.replace(char,spellOut[idx]);
-			delimStart=parsedSpeech.indexOf(char);
+
+// <<<<<<< Updated upstream
+function parseSpeech(input){
+	var parsedSpeech=input;
+
+	var specialChar=[".","(",")",":", ";",
+	"{", "}", "$", "?",
+	"!", "<", ">", "=", "\\", "/"];
+
+	var spellOut=[" period ", " left bracket ", " right bracket ", " colon ", " semi-colon ",
+	" left curly bracket ", " right curly bracket ", " dollar sign ", " question mark ",
+	" exclamation mark ", " less than ", " more than ", " is ", " slash ", " backslash "];
+
+	specialChar.map(function(char,idx){
+		var delimStart=parsedSpeech.indexOf(char);
+		while(delimStart>-1){
+			if(char == "!" && parsedSpeech[delimStart+1] == "="){
+				console.log(parsedSpeech.substr(delimStart, 2));
+				parsedSpeech=parsedSpeech.replace(parsedSpeech.substr(delimStart, 2)," not equal to ");
+			}else if(char === "=" && parsedSpeech[delimStart+1] === "="){
+				var length = (parsedSpeech.indexOf("=",delimStart+2) === delimStart+2)? 3:2;
+				console.log(parsedSpeech.substr(delimStart, length));
+				parsedSpeech=parsedSpeech.replace(parsedSpeech.substr(delimStart, length)," equal to ");
 			}
-			});
-		
-		console.log(parsedSpeech);
-		return parsedSpeech; 
-	}
+			else parsedSpeech=parsedSpeech.replace(char,spellOut[idx]);
+			delimStart=parsedSpeech.indexOf(char);
+		}
+	});
+
+	console.log(parsedSpeech);
+	return parsedSpeech;
+}
+// =======
+function parseSpeech(input){
+	var parsedSpeech;
+	return parsedSpeech;
+}
+// >>>>>>> Stashed changes
 
 	/** Turns on or off text-to-speech feature
 	@param: event
 	*/
-	function speechControl(e){
-		console.log('play/pause');
-		if(!isPaused){
+// <<<<<<< Updated upstream
+function speechControl(e){
+	console.log('play/pause');
+	if(!isPaused){
 			isPause=false;
 			synth.pause();
 			synth.cancel();
@@ -70,40 +81,133 @@ function control(){
     		//synth.resume();
     		return false;
     	}
-    }
+		}
 
-  	/**Store the value in the text area, which is the input from the user, into a variable
-  	*/  
-  	function saveCode(){
-
-  		storeCode = document.getElementById('code').value;}
-
-  		var num=0;
-
-	/**Store the value in the text-field, which is the input from the user, into a variable
-	*/
-	function saveCode(){
-		var storeCode = document.getElementById('code').value;
-	}
+/**Store the value in the text-field, which is the input from the user, into a variable
+*/
+function saveCode(){
+	var storeCode = document.getElementById('code').value;
+}
 
 	/**Send the input text to the OSC server to play the music out
 	*/
-	function sendServer() {
-		if(storeCode.length == null || storeCode.length == 0) {
-			console.log('User hasn\'t typed anything yet');
-			return;
-		}
-		console.log('playing: ' + storeCode);
-		var exec = require('child_process').exec, child;
-
-		child = exec('./client/SPi-reader.rb ' + storeCode,
-			function (error, stdout, stderr) {
-				console.log('stdout: ' + stdout);
-				console.log('stderr: ' + stderr);
-				if (error !== null) {
-					console.log('exec error: ' + error);
-				}
-			});
+function sendServer() {
+	if(storeCode.length == null || storeCode.length == 0) {
+		console.log('User hasn\'t typed anything yet');
+		return;
 	}
-};
-control();
+	console.log('playing: ' + storeCode);
+	var exec = require('child_process').exec, child;
+
+	child = exec('./client/SPi-reader.rb ' + storeCode,
+		function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});
+		control();
+	}
+
+// =======
+function speechControl(e){
+	console.log('play/pause');
+	if(!isPaused){
+		isPause=false;
+		synth.pause();
+		synth.cancel();
+	}
+	else{
+		console.log('preparing speech');
+		input = document.activeElement.value;//document.getElementById("code").value;
+		speech = new SpeechSynthesisUtterance(input);
+		console.log('speaking');
+		synth.speak(speech);
+    //synth.resume();
+	  return false;
+  }
+}
+
+var storeNode;
+var num = 0;
+
+/**Store the value in the text area, which is the input from the user, into a variable
+*/
+function saveCode(){
+    //Store the value in the text-field, which is the input from the user, into a variable
+	var storeCode = document.getElementById('code').value;
+	console.log('saveCode() called');
+	console.log(storeCode);
+//=======
+// Functions associated with forms and buttons on index.html are defined here.
+  sendServer(storeCode);
+}
+
+function submit(){
+  //Store the value in the text-field, which is the input from the user, into a variable
+	var storeCode = document.getElementById('text_field').value;
+  // >>>>>>> Stashed changes
+
+	/*if(storeCode.length!==0){
+		storeNode = document.createTextNode(storeCode);
+		document.getElementById('buffer1').appendChild(storeNode);
+<<<<<<< Updated upstream
+<<<<<<< HEAD
+		} */
+
+	/**Send the input text to the OSC server to play the music out
+	*/
+	sendServer(storeCode);
+}
+
+/*
+function sendServer() {
+	if(storeCode.length == null || storeCode.length == 0) {
+		console.log('User hasn\'t typed anything yet');
+		return;
+  }
+  console.log('playing: ' + storeCode);
+  var exec = require('child_process').exec, child;
+	child = exec('./client/SPi-reader.rb ' + storeCode,
+	// =======
+}
+*/
+
+function del(){
+	storeNode.parentNode.removeChild(storeNode);
+}
+
+function formatCommands(storeCode) {
+	var newCode = storeCode.replace(new RegExp( "\\n", "g" ), "; ");
+	newCode = "\"" + storeCode + "\"";
+	return newCode
+}
+
+function sendServer(storeCode) {
+  if(storeCode == null) {
+	  console.log('buffer is empty');
+	  return;
+	}
+
+	storeCode = formatCommands(storeCode);
+
+  console.log('playing: ' + storeCode);
+  var exec = require('child_process').exec, child;
+
+  child = exec('./client/SPi-reader.rb ' + storeCode,
+ // >>>>>>> Stashed changes
+    function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+	    console.log('stderr: ' + stderr);
+	    if (error !== null) {
+				console.log('exec error: ' + error);
+	    }
+// <<<<<<< Updated upstream
+    }
+	);
+	control();
+}
+// =======
+// >>>>>>> Stashed changes
+// >>>>>>> Stashed changes
